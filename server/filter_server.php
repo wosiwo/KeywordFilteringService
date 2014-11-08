332af1e89722c8ebacd328fb9ecab74c91fb942c
onceme <oncewosiwo@gmail.com>
2014-11-08 18:52:16 +0800

    Mod: add post way to receive parameter and let user can vist the file in webroot
    you need to vist the /filter  path to call the filter server,whether through GET OR POST.

--- server/filter_server.php
+++ server/filter_server.php
@@ -12,54 +12,71 @@ use My\Filter as F;
 class HttpServer extends Swoole\Protocol\HttpServer
 {
 
-	function onStart($serv, $worker_id = 0)
-	{
-		parent::onStart($serv, $worker_id);
-		$file = array('bin'=>realpath(__DIR__.'/dict_all.dat'),'source'=>realpath(__DIR__.'/dict_all.dat'));
-		$this->trie = new F\Trie($file);
-		$this->trie->nodes = $this->trie->getBinaryDict($file['bin']);
-	}
-
+    public function onStart($serv, $worker_id = 0)
+    {
+        parent::onStart($serv, $worker_id);
+        //启动时加载字典数据
+        $file = array('bin'=>realpath(__DIR__.'/dict_all.dat'),'source'=>realpath(__DIR__.'/dict_all.dat'));
+        $this->trie = new F\Trie($file);
+        $this->trie->nodes = $this->trie->getBinaryDict($file['bin']);
+    }
 
    /**
      * 处理请求
      * @param $request
      * @return Swoole\Response
      */
-    function onRequest(Swoole\Request $request)
+    public function onRequest(Swoole\Request $request)
     {
-        $response = new Swoole\Response;
-        $this->currentResponse = $response;
+        if ('/filter'==$request->meta['path']) {
 
+            $response = new Swoole\Response();
+            $this->currentResponse = $response;
+
+            //get param word
+            $word = self::getParam($request,'word');
+
+            if ($word) {
+                $result = $this->search($word);
+                // $result = self::changeCharset($result,'gbk','utf-8');
+                $result = json_encode($result);
+                $this->httpSend(200, $response, $result);
+
+                return $response;
+            } else {
+                $this->httpSend(200, $response, "parameters word no found");
+
+                return $response;
+            }
+
+        } else {
+            return parent::onRequest($request);
+        }
+
+    }
+
+    public static function getParam($request,$key='word')
+    {
         $uri = $request->meta['uri'];
         $uri = parse_url($uri);
-        // print_r($param);
-
-        if (isset($uri['query'])) {
-        	parse_str($uri['query'],$param);
-        	// print_r($param);
-        	$word  = self::getItem($param,'word');
-
-       		$result = $this->search($word);
-       		// $result = self::changeCharset($result,'gbk','utf-8');
-       		$result = json_encode($result);
-       		$this->httpSend(200, $response, $result);
-        	return $response;
-        }else{
-        	$this->httpSend(200, $response, "wosiwo");
-        	return $response;
+        $word = null;
+        if ('POST'==$request->meta['method']) {
+            $word = self::getItem( $request->post,'word');
+        } elseif (isset($uri['query'])) {
+            parse_str($uri['query'],$param);
+            $word  = self::getItem($param,'word');
         }
-       
 
-
-        
+        return $word;
     }
 
     public function search($word='')
     {
-    	// $word = self::changeCharset($word);
-    	$result = $this->trie->search($word);
-    	return $result;
+        // $word = self::changeCharset($word);
+        $ifIgnoreCase = true;       //是否忽略大小写
+        $result = $this->trie->search($word,$ifIgnoreCase);
+
+        return $result;
     }
 
     /**
@@ -68,7 +85,7 @@ class HttpServer extends Swoole\Protocol\HttpServer
      * @param Swoole\Response $response
      * @param string          $content
      */
-    function httpSend($code, Swoole\Response $response, $content = '')
+    public function httpSend($code, Swoole\Response $response, $content = '')
     {
         $response->send_http_status($code);
         $response->head['Content-Type'] = 'text/html';
@@ -77,23 +94,24 @@ class HttpServer extends Swoole\Protocol\HttpServer
 
     public static function getItem($array,$key='',$default=0)
     {
-    	return isset($array[$key])?$array[$key]:$default;
+        return isset($array[$key]) ? $array[$key] : $default;
     }
     //编码转换utf->gbk
-	public static function changeCharset($value, $from = 'utf-8', $to = 'gbk//ignore')
-	{
-		$result = array();
-		if(is_array($value)){
-			foreach($value as $k=> $v){
-				$k = self::changeCharset($k, $from, $to);
-				$result[$k] = self::changeCharset($v, $from, $to);
-			}
-		}else{
-			$value = (is_numeric($value) && floatval((int) $value) === floatval($value)) ? (int) $value : $value;
-			$result = is_string($value) ? iconv($from, $to, $value) : $value;
-		}
-		return $result;
-	}
+    public static function changeCharset($value, $from = 'utf-8', $to = 'gbk//ignore')
+    {
+        $result = array();
+        if (is_array($value)) {
+            foreach ($value as $k=> $v) {
+                $k = self::changeCharset($k, $from, $to);
+                $result[$k] = self::changeCharset($v, $from, $to);
+            }
+        } else {
+            $value = (is_numeric($value) && floatval((int) $value) === floatval($value)) ? (int) $value : $value;
+            $result = is_string($value) ? iconv($from, $to, $value) : $value;
+        }
+
+        return $result;
+    }
 
 }
 
